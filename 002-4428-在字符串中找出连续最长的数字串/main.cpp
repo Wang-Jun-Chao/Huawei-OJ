32a2d47149c95ea4c7b868ed713061f4b08c14e9
王俊超 <shining-glory@qq.com>
2016-01-15 18:23:20 +0800

    commit

--- 002-4428-在字符串中找出连续最长的数字串/main.cpp
+++ 002-4428-在字符串中找出连续最长的数字串/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include "OJ.h"
 
 using namespace std;
@@ -10,13 +11,23 @@ int main() {
 
     cout << input << endl;
 
-    unsigned int rs = Continumax(&p, input);
+    unsigned int rs = Continumax(&p, NULL);
+    cout << rs << ", " << &p << ", |" << p << "|" <<endl;
 
-    cout << rs << ", " << cout << *p << ", "  << p << endl;
+    rs = Continumax(&p, input);
+     cout << rs << ", " << &p << ", |" << p << "|" <<endl;
 
     rs = Continumax(&p, NULL);
+    cout << rs << ", " << &p << ", |" << p << "|" <<endl;
+/*
+    char input2[] = "abcd12345ed125ss123058789";
+    char * p2;
+    unsigned int rs2 = Continumax(&p2, input2);
+    cout << rs2 << ", " << &p2 << ", " << p2 << endl;
 
-    cout << rs << ", " << cout << *p << endl;
-
+    char input3[] = "abcd12345ss54761";
+    rs = Continumax(&p, input3);
+    cout << rs << ", " << &p << ", " << p << endl;
+*/
     return 0;
 }

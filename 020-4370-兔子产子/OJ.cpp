93a27fe98739172789d90e7a17f06b1bdb934ebc
王俊超 <shining-glory@qq.com>
2016-01-21 10:23:12 +0800

    commit

---
+++ 020-4370-兔子产子/OJ.cpp
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "oj.h"
+// 功能：获取第nValue1个月出生多少对兔子
+// 输入： nValue1为正整数
+// 输出：无
+// 返回：第nValue1个月出生多少对兔子
+
+
+unsigned int GetBirthRabbitNum(unsigned int  nValue1)
+{
+	return 0;
+}

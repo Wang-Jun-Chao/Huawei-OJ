6a6aa8b910e8060eb51881c53242fc4beb917eab
Wang-Jun-Chao <shining-glory@qq.com>
2016-02-01 16:30:19 +0800

    commit

--- 040-4390-字符串分割/main.cpp
+++ 040-4390-字符串分割/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include "OJ.h"
 
 using namespace std;
 
+void testCase01()
+{
+	AddString("434353f");
+	AddString("ABCDEFG1234");
+	AddString("1234567890");
+	//AddString("12345678");
+
+	//printMatrix();
+
+	char strRst[][9] = {"434353f0","ABCDEFG1","23400000","12345678","90000000"};
+
+	cout << (5 == GetLength()) << endl;
+	cout << (0 == ArrCmp(strRst,5)) << endl;
+}
+
 int main() {
     cout << "Hello world!" << endl;
+
+    testCase01();
+
     return 0;
 }

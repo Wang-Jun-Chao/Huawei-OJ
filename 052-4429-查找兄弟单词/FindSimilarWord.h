c3cfdcab5d545fc7379748e5950b0322cf6bcc41
Wang-Jun-Chao <shining-glory@qq.com>
2016-02-01 14:38:45 +0800

    commit

---
+++ 052-4429-查找兄弟单词/FindSimilarWord.h
@@ -0,0 +1,11 @@
+#ifndef FINDSIMILARWORD_H_INCLUDED
+#define FINDSIMILARWORD_H_INCLUDED
+
+int AddOneWord (char* Word);
+int FindSimilarWordNum (char* Word);
+int FindOneSimilarWord (char* Word, int Seq, char* SimilarWord);
+void ClearAllWords(void);
+
+void printDic();
+
+#endif // FINDSIMILARWORD_H_INCLUDED
--- 052-4429-查找兄弟单词/main.cpp
+++ 052-4429-查找兄弟单词/main.cpp
@@ -1,8 +1,59 @@
 #include <iostream>
+#include <string.h>
+
+#include "FindSimilarWord.h"
 
 using namespace std;
+char *Test_Word[8] = {
+    "mock",
+    "aabc",
+    "abc",
+    "ckom",
+    "bcaa",
+    "abca",
+    "abca",
+    "abcaaaa",
+};
 
 int main() {
     cout << "Hello world!" << endl;
+
+    int rst = 0;
+    rst = AddOneWord(Test_Word[0]);
+    cout << rst << endl;
+    rst = AddOneWord(Test_Word[1]);
+    cout << rst << endl;
+    rst = AddOneWord(Test_Word[2]);
+    cout << rst << endl;
+    rst = AddOneWord(Test_Word[3]);
+    cout << rst << endl;
+    rst = AddOneWord(Test_Word[4]);
+    cout << rst << endl;
+    rst = AddOneWord(Test_Word[5]);
+    cout << rst << endl;
+    rst = AddOneWord(Test_Word[6]);
+    cout << rst << endl;
+     rst = AddOneWord(Test_Word[7]);
+    cout << rst << endl << endl;
+
+    //printDic();
+
+    int Expectnumber = 1;
+    int ActualNumber;
+    int Ret;
+
+    ActualNumber = FindSimilarWordNum (Test_Word[0]);
+    cout << Expectnumber << " " << ActualNumber << endl;
+
+    char *ExpectWord = {"bcaa"};
+    char SimilarWord[51] = {'\0'};
+    int Seq = 2;
+    Ret = FindOneSimilarWord (Test_Word[1], Seq, SimilarWord);
+
+    cout << 0 << " " << Ret << endl;
+    cout << ExpectWord << "=" << SimilarWord << endl;
+    cout << (!memcmp(ExpectWord, SimilarWord, strlen(ExpectWord) + 1)) << endl;
+
     return 0;
 }
+
